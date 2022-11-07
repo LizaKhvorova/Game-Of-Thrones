aa84ea678df71c5b88f1d50ea31508fb2f7d1e6c
Khvorova_Liza <liza.khvorova@gmail.com>
2022-11-07 10:43:33 +0300

    init

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './spinner.css'
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-eclipse-orjt11ubxdq"><div class="ldio-aeze71tybqj">
+      <div></div>
+    </div></div>
+    )
+}
+
+
+export default Spinner
